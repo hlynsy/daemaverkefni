0cb6f440545040f638f227ba3793ca503f5c354c
Hlynur Freyr <hlynurh18@ru.is>
2018-09-04 14:16:55 +0000

    Klárað

--- Documents/skóli/forritun/daemaverkefni/Assignment5/max_int.py
+++ Documents/skóli/forritun/daemaverkefni/Assignment5/max_int.py
@@ -1,10 +1,14 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
 
-number_list = []
+max_int = 0
 
 while num_int >=0:
-    number_list.append(num_int)
+    if num_int > max_int:
+        max_int = num_int
+    
+    num_int = int(input("Input a number: ")) 
+    
 
-max_int = max(number_list)
+
 print("The maximum is", max_int)    # Do not change this line
